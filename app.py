c1bcdfa78c6abd92cab021cafc71ddba23a75107
tamara azzam <tamaraazzam@tamaras-Air.lan>
2025-12-14 19:03:21 -0600

    app comments

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import os
 from flask import Flask, render_template, g
 from configparser import ConfigParser
-# might not use this function
 from database.handler import get_db_connection_for_request
 from routes.data_entry import data_entry_bp
 from routes.evaluation import evaluation_bp
 from routes.querying import querying_bp
 
+#create and configure flask
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'databases'
+    #load config, handle errors
     try:
         config = ConfigParser()
         config_path = os.path.join(app.root_path, 'config.txt')
@@ -30,11 +31,11 @@ def create_app():
     app.register_blueprint(data_entry_bp)
     app.register_blueprint(evaluation_bp)
     app.register_blueprint(querying_bp)
-
+    #main route
     @app.route('/')
     def index():
         return render_template('index.html') 
-
+    #close db connection after each request
     @app.teardown_appcontext
     def close_db_connection(exception):
         db = g.pop('db', None)
@@ -42,7 +43,7 @@ def create_app():
             db.close()
 
     return app
-
+#run
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
